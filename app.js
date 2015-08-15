85993164d13ee6d968b4682f5fa094623f5fb7ff
Cody Moss <codayblue@users.noreply.github.com>
2015-08-15 12:16:22 -0700

    Removed view folder and view engine since front end will take care of views.

--- app.js
+++ app.js
@@ -7,10 +7,6 @@ var bodyParser = require('body-parser');
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hjs');
-
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
