5a8131c8462e7ee00123bd6ca30294fb75a0ce27
Cody Moss <codayblue@users.noreply.github.com>
2015-08-16 10:12:23 -0700

    Finished backend API to get a list of seasons for the show that is passed in also updated both calls to use sync so that we get the data back first try

--- routes/api.js
+++ routes/api.js
@@ -1,6 +1,10 @@
 var fs = require('fs');
 var vidTbl = [];
 var vidRow = {};
+var vids = [];
+var seasons = [];
+var show = '';
+var season = '';
 
 module.exports = function(app)
 {
@@ -9,30 +13,42 @@ module.exports = function(app)
   });
 
   // List out videos based on directory and grab icon.
-  app.get('/listVideos', function(req, res){
-    fs.readdir('./videos/', function(err, files){
-      if(err){
-        console.log(err);
-        return;
-      }
+  app.get('/listGallery', function(req, res){
+    var files = fs.readdirSync('./videos/');
       files.forEach(function(file){
-        fs.stat('./videos/' + file, function(err, status){
-          if(err){
-            console.log(err);
-            return;
-          }
-          if(status.isDirectory()){
+        var stats = fs.statSync('./videos/' + file);
+          if(stats.isDirectory())
+          {
             vidRow = {
               'title': file,
               'imgSrc': '../videos/' + file + '/image.png'
             };
             vidTbl.push(vidRow);
           }
-        });
       });
-    });
     res.send(vidTbl);
+    // clear out old data
     vidTbl = [];
     vidRow = {};
+  });
+
+  //get the title back and go through and get the Seasons or videos.
+  app.get('/listSeasons/:show', function(req, res){
+    show = req.params.show;
+    var files = fs.readdirSync('./videos/' + show);
+      files.forEach(function(file){
+        var stats =  fs.statSync('./videos/' + show + '/' + file);
+          if(stats.isDirectory())
+          {
+            seasons.push(file);
+          }
+      });
+    res.send({
+      'show': show,
+      'seasons': seasons
+    });
+    //clear out data
+    show = '';
+    seasons = [];
   });
 };
